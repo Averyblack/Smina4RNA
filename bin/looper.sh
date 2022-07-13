873d6b8146cf5ae0a71c8708ff3c6d18526d2d26
Averyblack <63167268+Averyblack@users.noreply.github.com>
2022-07-13 15:40:54 +0200

    Add files via upload

--- bin/looper.sh
+++ bin/looper.sh
@@ -2,6 +2,11 @@
 
 ls $1 > temp_data_files.txt
 
-parallel -q './B-single-docking.sh' $1/{} $2{} :::: temp_data_files.txt
+parallel -q './B-single-docking.sh' $1/{} $2/{} :::: temp_data_files.txt
 
 rm temp_data_files.txt
+
+#ls $2 > temp_output_files.txt
+
+#parallel python3 compute.py -p $2{} :::: temp_output_files.txt
+python3 compute_mean.py -p $2/
