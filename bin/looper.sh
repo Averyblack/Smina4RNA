80b264d60296fd4768bc3750af120aa821144449
Averyblack <63167268+Averyblack@users.noreply.github.com>
2022-07-14 14:47:36 +0200

    Update looper.sh

--- bin/looper.sh
+++ bin/looper.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+#Master script for
+
+# 1 - path to training data
+# 2 - path to output dir
+
+#Creates the files with the names of the data folders to loop over
 ls $1 > temp_data_files.txt
 
-parallel -q './B-single-docking.sh' $1/{} $2/{} :::: temp_data_files.txt
+# Runs B-single-docking.sh parallelly for all training data folders
+parallel --jobs 6 --progress -q './B-single-docking.sh' $1/{} $2/{} :::: temp_data_files.txt
 
+# Removes the temp file
 rm temp_data_files.txt
 
-#ls $2 > temp_output_files.txt
-
-#parallel python3 compute.py -p $2{} :::: temp_output_files.txt
+# Calculate the mean of the results
 python3 compute_mean.py -p $2/
