9f43ada2f1065613304b46aed2d85672a43204df
Averyblack <63167268+Averyblack@users.noreply.github.com>
2022-07-15 14:36:35 +0200

    Update looper.sh

--- bin/looper.sh
+++ bin/looper.sh
@@ -8,8 +8,10 @@
 #Creates the files with the names of the data folders to loop over
 ls $1 > temp_data_files.txt
 
+
 # Runs B-single-docking.sh parallelly for all training data folders
-parallel --jobs 6 --progress -q './B-single-docking.sh' $1/{} $2/{} :::: temp_data_files.txt
+parallel xz --decompress $1/{}/rna.pdbqt.xz :::: temp_data_files.txt
+parallel --jobs 3 --progress -q './B-single-docking.sh' $1/{} $2/{} :::: temp_data_files.txt
 
 # Removes the temp file
 rm temp_data_files.txt
